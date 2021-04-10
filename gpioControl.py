820666cc943c6f85c8718305f8f25cc0829ae6f2
unknown <huynhtrunghieu2000@gmail.com>
2021-04-10 15:51:24 +0700

    update

--- gpioControl.py
+++ gpioControl.py
@@ -9,6 +9,7 @@ p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
 p.start(2.5)  # Initialization
 try:
     while True:
+        # 7-> 90*
         p.ChangeDutyCycle(7)
         time.sleep(2)
         p.ChangeDutyCycle(2)
