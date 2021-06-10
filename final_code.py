3bc4e99dfd315db8bf6cc4b9d73a8274512bcb77
unknown <huynhtrunghieu2000@gmail.com>
2021-06-11 00:23:24 +0700

    send code to Pi

--- final_code.py
+++ final_code.py
@@ -7,32 +7,6 @@ from threading import Thread
 import time
 import firebase
 
-GPIO.setmode(GPIO.BOARD)
-
-# UltraSonic 1
-US_TRIG1 = 8
-US_ECHO1 = 10
-GPIO.setup(US_TRIG1, GPIO.OUT)
-GPIO.setup(US_ECHO1, GPIO.IN)
-# UltraSonic 2
-US_TRIG2 = 16
-US_ECHO2 = 18
-GPIO.setup(US_TRIG2, GPIO.OUT)
-GPIO.setup(US_ECHO2, GPIO.IN)
-# UltraSonic 3
-US_TRIG3 = 22
-US_ECHO3 = 24
-GPIO.setup(US_TRIG3, GPIO.OUT)
-GPIO.setup(US_ECHO3, GPIO.IN)
-# Servo 1
-SERVO1 = 11
-GPIO.setup(SERVO1, GPIO.OUT)
-servo1 = GPIO.PWM(SERVO1, 50)
-
-# Servo 2
-SERVO2 = 13
-GPIO.setup(SERVO2, GPIO.OUT)
-servo2 = GPIO.PWM(SERVO2, 50)
 
 status = "idle"
 humanStanding = 0
@@ -96,6 +70,32 @@ def check_percent_garbage(trig, echo):
 
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BOARD)
+
+    # UltraSonic 1
+    US_TRIG1 = 8
+    US_ECHO1 = 10
+    GPIO.setup(US_TRIG1, GPIO.OUT)
+    GPIO.setup(US_ECHO1, GPIO.IN)
+    # UltraSonic 2
+    US_TRIG2 = 16
+    US_ECHO2 = 18
+    GPIO.setup(US_TRIG2, GPIO.OUT)
+    GPIO.setup(US_ECHO2, GPIO.IN)
+    # UltraSonic 3
+    US_TRIG3 = 22
+    US_ECHO3 = 24
+    GPIO.setup(US_TRIG3, GPIO.OUT)
+    GPIO.setup(US_ECHO3, GPIO.IN)
+    # Servo 1
+    SERVO1 = 11
+    GPIO.setup(SERVO1, GPIO.OUT)
+    servo1 = GPIO.PWM(SERVO1, 50)
+
+    # Servo 2
+    SERVO2 = 13
+    GPIO.setup(SERVO2, GPIO.OUT)
+    servo2 = GPIO.PWM(SERVO2, 50)
     try:
         threadCheckPercent = threading.Thread(
             target=check_percent_garbage, args=(US_TRIG2, US_ECHO2))
